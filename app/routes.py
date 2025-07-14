944e4d5663b4e93f920614ea857db282aacacc14
ZXC-DeSh <egorkorobkin37@gmail.com>
2025-07-14 14:39:45 +0300

    + gitignore

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Egor'}
+    posts = [
+            {
+                'author' : {'username' : 'Олег'},
+                'body' : 'Присутствовал на лекции по Математике.'
+            },
+            {
+                'author' : {'username' : 'Анна'},
+                'body' : 'Отсутствовала на семинаре по Физике (болезнь).'
+            },
+            {
+                'author' : {'username' : 'Иван'},
+                'body' : 'Присутствовал на практическом занятии по Программированию.'
+            }
+        ]
+
+    return render_template('index.html', title='Главная', user=user, posts=posts)
